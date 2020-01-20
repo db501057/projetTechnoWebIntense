5b13aff70761a3fdb5d682b5a6e2c1089b8026ee
b.dekeyser <benjdek@gmail.com>
2020-01-20 09:38:48 +0100

    bug

--- src/app/pages/home/detail/detail.component.ts
+++ src/app/pages/home/detail/detail.component.ts
@@ -11,26 +11,25 @@ export class DetailComponent implements OnInit {
 
   term: string;
   data: any[];
-  artist: string
+  artist: string;
 
   constructor(private detailsService: DetailsService) {
 
   }
 
   ngOnInit() {
-    console.log(window.location.pathname.split("/")[4]);
-    this.artist = window.location.pathname.split("/")[4];
+    console.log(window.location.pathname.split('/')[4]);
+    this.artist = window.location.pathname.split('/')[4];
     this.getArtist();
   }
 
   private getArtist() {
     this.detailsService.getArtist(this.artist).subscribe(data => {
       this.data = data;
-      if (data.length == 0){
+      if (data.length === 0){
         this.data = [];
       }
       });
-      console.log(this.data)
     }
 
 }
